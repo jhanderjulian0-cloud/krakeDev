2d8ca53b373d5106f9d06a16b5d33f1042386255
DESKTOP-AOR2EU4\Usuario <jhandersarango0@gmail.com>
2025-09-23 20:57:20 -0500

    Subiendo mi proyecto inicial

--- ejercicio_six.js
+++ ejercicio_six.js
@@ -1,3 +1,6 @@
 saludar=function(){
-    console.log("hola amigooo")
+    console.log("hola amigooo");
+    let cmpImagen;
+    cmpImagen=document.getElementById("saludo");
+    cmpImagen.src="./imagenes/ganar.png";
 }
