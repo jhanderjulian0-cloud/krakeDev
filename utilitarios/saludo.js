6f9b224c2120fc59a2a53ebcbedfcd4db7f5e253
Sixjhander <jhandersarango0@gmail.com>
2025-10-01 19:17:42 -0500

    actualizando recuperar INT Y FLOAT

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -9,4 +9,16 @@ recuperarTexto=function(idComponente){
 saludar=function(){
     let nombre =recuperarTexto("txtNombre");
     let apellido = recuperarTexto("txtApellido");
+    let edad = recuperarInt("txtEdad");
+     let estatura = recuperarFloat("txtEstatura");
+}
+recuperarInt=function(){
+    let valorCaja = recuperarTexto(idComponente);
+    let valorEntero= parseInt(valorCaja);
+    return valorEntero;
+}
+recuperarFloat=function(){
+    let valorCaja = recuperarTexto(idComponente);
+    let valorFloat= parseFloat(valorCaja);
+    return valorFloat;
 }
